d235b1dd1e40a0cd7602a663df2c2d5eab643bb7
Sauleteh <sauljgz21@yahoo.es>
2024-05-08 13:06:33 +0200

    Circle Dodger: Cronómetro

---
+++ src/circle-dodger/js/Chronometer.js
@@ -0,0 +1,23 @@
+export class Chronometer
+{
+    constructor() {
+        this.startTime = 0;
+    }
+
+    /** Guarda el tiempo de comienzo
+     * @param {number} intervalId - Identificador de la función que será llamada periódicamente una vez se haga start(), es decir, el valor devuelto por setInterval(<función>, <intervalo>)
+    */
+    start(intervalId) {
+        this.startTime = performance.now();
+        this.intervalId = intervalId;
+    }
+ç
+    // Detiene la ejecución peródica de la función asignada en start()
+    stop() { clearInterval(this.intervalId); }
+
+    // Devuelve el tiempo transcurrido desde el inicio
+    getElapsedTime() { return performance.now() - this.startTime; }
+
+    // Convertidor de números con ceros a la izquierda
+    zeroPad(num, places) { return String(num).padStart(places, '0'); }
+}
--- src/circle-dodger/js/game.js
+++ src/circle-dodger/js/game.js
@@ -2,11 +2,12 @@ import { CircleFigure } from "./CircleFigure.js";
 import { FPSControllerV2 } from "./FPSControllerV2.js";
 import { RectangleFigure } from "./RectangleFigure.js";
 import { Direction } from "./Direction.js";
+import { Chronometer } from "./Chronometer.js";
 
 document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.querySelector("canvas.board");
     const ctx = canvas.getContext("2d");
-
+    const chronoText = document.querySelector(".chronometer");
     const cbClickInsteadOfHolding = document.querySelector("#cbClickInsteadOfHolding");
 
     canvas.onselectstart = function() { return false; } // Evitar que se seleccione texto al hacer click y arrastrar
@@ -14,25 +15,32 @@ document.addEventListener('DOMContentLoaded', function() {
     canvas.height = 600;
 
     const fpsController = new FPSControllerV2(60);
+    const chronoObj = new Chronometer();
 
-    const player = new CircleFigure(canvas.width / 2, canvas.height / 2, 40, "blue");
+    let player = null;
     let isMouseDownOnPlayer = false;
     let isMouseClickOnPlayer = false;
 
-    const enemies = [
-        { figure: new RectangleFigure(100, 100, 50, 50, "red"), direction: new Direction(1, 1) },
-        { figure: new RectangleFigure(200, 200, 50, 50, "red"), direction: new Direction(-1, -1) },
-        { figure: new RectangleFigure(300, 300, 50, 50, "red"), direction: new Direction(1, -1) },
-        { figure: new RectangleFigure(400, 400, 50, 50, "red"), direction: new Direction(-1, 1) }
-    ];
+    let enemies = [];
+
+    let isGameOver = false;
+    let isGameStarted = false;
 
     function initEvents() {
-        //stopEvents(); // Detener eventos anteriores
+        stopEvents(); // Detener eventos anteriores
         console.log("Initializing events...");
         canvas.addEventListener("click", onClickListener);
         canvas.addEventListener("mousedown", onMouseDown);
         canvas.addEventListener("mouseup", onMouseUp);
         canvas.addEventListener("mousemove", onMouseMove);
+    }
+
+    function stopEvents() {
+        console.log("Stopping events...");
+        canvas.removeEventListener("click", onClickListener);
+        canvas.removeEventListener("mousedown", onMouseDown);
+        canvas.removeEventListener("mouseup", onMouseUp);
+        canvas.removeEventListener("mousemove", onMouseMove);
     }
 
     function onClickListener(e) {
@@ -56,11 +64,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function onMouseMove(e) {
         console.log("Mouse move event");
+
         if (cbClickInsteadOfHolding.checked) {
             // Al hacer click en el jugador, se mueve hacia la posición del mouse hasta que se haga otro click
             if (isMouseClickOnPlayer) {
                 player.x = e.offsetX;
                 player.y = e.offsetY;
+                if (!isGameStarted) {
+                    isGameStarted = true;
+                    chronoText.textContent = "00:00";
+                    chronoObj.start(setInterval(everySecond, 1000)); // Actualizar el cronómetro cada segundo (1000ms));
+                }
             }
         }
         else {
@@ -68,6 +82,11 @@ document.addEventListener('DOMContentLoaded', function() {
             if (isMouseDownOnPlayer) {
                 player.x = e.offsetX;
                 player.y = e.offsetY;
+                if (!isGameStarted) {
+                    isGameStarted = true;
+                    chronoText.textContent = "00:00";
+                    chronoObj.start(setInterval(everySecond, 1000)); // Actualizar el cronómetro cada segundo (1000ms));
+                }
             }
         }
     }
@@ -96,8 +115,7 @@ document.addEventListener('DOMContentLoaded', function() {
             if (player.isCollidingWith(enemy.figure) || player.isOutOfCanvas(canvas))
             {
                 console.log("Collision detected!");
-                //alert("¡Has perdido!");
-                //window.location.reload();
+                isGameOver = true;
             }
         });
     }
@@ -112,21 +130,62 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    function resetGame() {
+        initEvents();
+
+        isGameOver = false;
+        isGameStarted = false;
+        isMouseDownOnPlayer = false;
+        isMouseClickOnPlayer = false;
+        player = new CircleFigure(canvas.width / 2, canvas.height / 2, 40, "blue");
+        enemies = [
+            { figure: new RectangleFigure(100, 100, 70, 70, "red"), direction: new Direction(4, 4) },
+            { figure: new RectangleFigure(100, canvas.height - 100 - 70, 70, 60, "red"), direction: new Direction(-5, -2) },
+            { figure: new RectangleFigure(canvas.width - 100 - 70, 100, 35, 70, "red"), direction: new Direction(4, -3) },
+            { figure: new RectangleFigure(canvas.width - 100 - 70, canvas.height - 100 - 70, 120, 20, "red"), direction: new Direction(-3, 5) }
+        ];
+    }
+
+    function everySecond() {
+        const msTime = chronoObj.getElapsedTime();
+        const seconds = chronoObj.zeroPad(Math.floor(msTime / 1000) % 60, 2);
+        const minutes = chronoObj.zeroPad(Math.floor(msTime / 1000 / 60), 2);
+        chronoText.textContent = `${minutes}:${seconds}`;
+    }
+
+    function onGameOver() {
+        // Al terminar, el tiempo se mostrará en milisegundos
+        chronoObj.stop();
+        const msTime = chronoObj.getElapsedTime();
+        const miliseconds = chronoObj.zeroPad(Math.floor(msTime) % 1000, 3);
+        const seconds = chronoObj.zeroPad(Math.floor(msTime / 1000) % 60, 2);
+        const minutes = chronoObj.zeroPad(Math.floor(msTime / 1000 / 60), 2);
+        chronoText.textContent = `${minutes}:${seconds}.${miliseconds}`;
+    }
+
     function draw(now) {
         window.requestAnimationFrame(draw);
         if (!fpsController.shouldContinue(now)) return;
-
+        
         clearCanvas();
         drawPlayer();
         drawEnemies();
 
+        if (!isGameStarted) return;
         checkCollisions();
-        updateEnemies();
+        if (isGameOver) { // Mientras no se juega...
+            console.log("Game over!");
+            onGameOver();
+            resetGame();
+        }
+        else { // Mientras se juega...
+            updateEnemies();
+        }
 
         fpsController.updateLastTime(now);
     }
 
-    initEvents();
+    resetGame();
     draw();
 });
 
@@ -136,7 +195,7 @@ document.addEventListener('DOMContentLoaded', function() {
  * - [ ] Implementar backend
  *       - [ ] Guardar puntuaciones
  *       - [ ] Consultar puntuaciones
- * - [ ] Implementar game over: Al colisionar, se muestra un mensaje de game over y se reinicia el juego (el cronómetro no se reinicia hasta que se empieza otra partida)
+ * - [X] Implementar game over: Al colisionar, se muestra un mensaje de game over y se reinicia el juego (el cronómetro no se reinicia hasta que se empieza otra partida)
  * - [ ] Implementar niveles: Cada cierto tiempo, aumenta la dificultad
  * - [ ] Implementar power-ups: Al colisionar con un power-up, se obtiene un beneficio
  * - [ ] Implementar enemigos más inteligentes: Los enemigos persiguen al jugador
