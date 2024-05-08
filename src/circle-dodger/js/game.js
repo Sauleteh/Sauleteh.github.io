9ca40316f11e6b6a123de746ce5b7b1614abde0e
Sauleteh <sauljgz21@yahoo.es>
2024-05-08 16:41:58 +0200

    TODO Circle Dodger: Implementación de niveles

--- src/circle-dodger/js/game.js
+++ src/circle-dodger/js/game.js
@@ -7,7 +7,8 @@ import { Chronometer } from "./Chronometer.js";
 document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.querySelector("canvas.board");
     const ctx = canvas.getContext("2d");
-    const chronoText = document.querySelector(".chronometer");
+    const chronoText = document.querySelector("label.chronometer");
+    const levelText = document.querySelector("label.level");
     const cbClickInsteadOfHolding = document.querySelector("#cbClickInsteadOfHolding");
 
     canvas.onselectstart = function() { return false; } // Evitar que se seleccione texto al hacer click y arrastrar
@@ -25,6 +26,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let isGameOver = false;
     let isGameStarted = false;
+    let level = 1;
+    let enemySpeedMultiplier = 1;
+    let isLevelTwoEnemyAdded = false;
+    let warningInterval = null; // Intervalo para cambiar el estado del warning
+    let warningState = false; // Estado del warning para hacer el efecto de parpadeo
 
     function initEvents() {
         stopEvents(); // Detener eventos anteriores
@@ -44,26 +50,26 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function onClickListener(e) {
-        console.log("Click event");
+        // console.log("Click event");
 
         // Si se hace click en el jugador, se activa/desactiva la variable para moverlo
         if (e.button === 0 && player.isPointInside(e.offsetX, e.offsetY)) isMouseClickOnPlayer = !isMouseClickOnPlayer;
     }
 
     function onMouseDown(e) {
-        console.log("Mouse down event");
+        // console.log("Mouse down event");
 
         // Si al pulsar, el ratón está sobre el jugador, se activa la variable para moverlo
         if (e.button === 0 && player.isPointInside(e.offsetX, e.offsetY)) isMouseDownOnPlayer = true;
     }
 
     function onMouseUp(e) {
-        console.log("Mouse up event");
+        // console.log("Mouse up event");
         if (e.button === 0) isMouseDownOnPlayer = false;
     }
 
     function onMouseMove(e) {
-        console.log("Mouse move event");
+        // console.log("Mouse move event");
 
         if (cbClickInsteadOfHolding.checked) {
             // Al hacer click en el jugador, se mueve hacia la posición del mouse hasta que se haga otro click
@@ -115,15 +121,22 @@ document.addEventListener('DOMContentLoaded', function() {
             if (player.isCollidingWith(enemy.figure) || player.isOutOfCanvas(canvas))
             {
                 console.log("Collision detected!");
-                isGameOver = true;
+                // TODO: isGameOver = true;
             }
         });
     }
 
     function updateEnemies() {
+        if (level === 2 && !isLevelTwoEnemyAdded) {
+            enemies.push({ figure: new RectangleFigure(canvas.width / 2, canvas.height / 2, 40, 40, "red"), direction: new Direction(2, 6) });
+            isLevelTwoEnemyAdded = true;
+        }
         enemies.forEach(enemy => {
-            enemy.figure.x += enemy.direction.dX;
-            enemy.figure.y += enemy.direction.dY;
+            if (level === 1) enemySpeedMultiplier = Math.max(1, Math.min(5, (chronoObj.getElapsedTime() / 1000 / 5))); // A los 25s de juego, el multiplicador será 5
+            else if (level === 2) enemySpeedMultiplier = Math.max(4, Math.min(5, 5 - ((chronoObj.getElapsedTime() - 30000) / 1000 / 5))); // En 5s, el multiplicador pasará de 5 a 4
+            console.log(enemySpeedMultiplier);
+            enemy.figure.x += enemy.direction.dX * enemySpeedMultiplier;
+            enemy.figure.y += enemy.direction.dY * enemySpeedMultiplier;
 
             if (enemy.figure.x < 0 || enemy.figure.x + enemy.figure.width > canvas.width) enemy.direction.dX *= -1;
             if (enemy.figure.y < 0 || enemy.figure.y + enemy.figure.height > canvas.height) enemy.direction.dY *= -1;
@@ -137,7 +150,12 @@ document.addEventListener('DOMContentLoaded', function() {
         isGameStarted = false;
         isMouseDownOnPlayer = false;
         isMouseClickOnPlayer = false;
-        player = new CircleFigure(canvas.width / 2, canvas.height / 2, 40, "blue");
+        level = 1;
+        enemySpeedMultiplier = 1;
+        isLevelTwoEnemyAdded = false;
+        warningInterval = null;
+        warningState = false;
+        player = new CircleFigure(canvas.width / 2, canvas.height / 2, 35, "blue");
         enemies = [
             { figure: new RectangleFigure(100, 100, 70, 70, "red"), direction: new Direction(4, 4) },
             { figure: new RectangleFigure(100, canvas.height - 100 - 70, 70, 60, "red"), direction: new Direction(-5, -2) },
@@ -163,6 +181,32 @@ document.addEventListener('DOMContentLoaded', function() {
         chronoText.textContent = `${minutes}:${seconds}.${miliseconds}`;
     }
 
+    function updateLevel() {
+        if (chronoObj.getElapsedTime() < 30000) level = 1; // 0-30s
+        else if (chronoObj.getElapsedTime() < 60000) level = 2; // 30-60s
+        else if (chronoObj.getElapsedTime() < 90000) level = 3; // 60-90s
+        else if (chronoObj.getElapsedTime() < 120000) level = 4; // 90-120s
+        else level = 5; // 120s en adelante
+
+        levelText.textContent = `Nivel ${level}`;
+    }
+
+    function drawEnemyWarning() {
+        // Si se está en el nivel 1 a partir de los 27s, se mostrará un signo de exclamación indicando que aparecerá un nuevo enemigo
+        if (level === 1 && !isLevelTwoEnemyAdded && chronoObj.getElapsedTime() > 27000) {
+            if (warningInterval === null) warningInterval = setInterval(() => warningState = !warningState, 250);
+            ctx.font = "40px Arial";
+            if (warningState) ctx.fillStyle = "red";
+            else ctx.fillStyle = "white";
+            ctx.textAlign = "center";
+            ctx.fillText("!", canvas.width / 2, canvas.height / 2);
+        }
+        else {
+            clearInterval(warningInterval);
+            warningInterval = null;
+        }
+    }
+
     function draw(now) {
         window.requestAnimationFrame(draw);
         if (!fpsController.shouldContinue(now)) return;
@@ -170,6 +214,7 @@ document.addEventListener('DOMContentLoaded', function() {
         clearCanvas();
         drawPlayer();
         drawEnemies();
+        drawEnemyWarning();
 
         if (!isGameStarted) return;
         checkCollisions();
@@ -180,6 +225,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         else { // Mientras se juega...
             updateEnemies();
+            updateLevel();
         }
 
         fpsController.updateLastTime(now);
@@ -197,8 +243,28 @@ document.addEventListener('DOMContentLoaded', function() {
  *       - [ ] Consultar puntuaciones
  * - [X] Implementar game over: Al colisionar, se muestra un mensaje de game over y se reinicia el juego (el cronómetro no se reinicia hasta que se empieza otra partida)
  * - [ ] Implementar niveles: Cada cierto tiempo, aumenta la dificultad
+ *       - [X] Nivel 1 (0-30s): De 1 a 5 de multiplicador de velocidad de enemigos (máx. 5 a los 25s)
+ *       - [X] Nivel 2 (30-60s): Bajada de multiplicador de velocidad de enemigos a 4 (transición de 5s) + un nuevo enemigo + power-downs
+ *       - [ ] Nivel 3 (60-90s): Aparición de áreas circulares que matan al jugador si entra en ellas cuando se activen
+ *             - Propiedades área circular: Tiempo de activación 3s, intervalo de aparición 6s, radio 60
+ *       - [ ] Nivel 4 (90-120s): Las áreas circulares ocurren con mayor frecuencia y a mayor velocidad
+ *             - Propiedades área circular: Tiempo de activación 2s, intervalo de aparición 4s, radio 60
+ *       - [ ] Nivel 5 (120s en adelante): Subida de multiplicador de velocidad de enemigos de 4 a 6 (transición de 10s) + áreas circulares más grandes
+ *             - Propiedades área circular: Tiempo de activación 2s, intervalo de aparición 4s, radio 80
  * - [ ] Implementar power-ups: Al colisionar con un power-up, se obtiene un beneficio
- * - [ ] Implementar enemigos más inteligentes: Los enemigos persiguen al jugador
+ *       - [ ] Cada 15 segundos (y durante 10 segundos), aparece un power-up en una posición aleatoria
+ *             - Power-up 1: Disminuye el radio del jugador (permanente)
+ *             - Power-up 2: Disminuye el tamaño de los enemigos (permanente)
+ *             - Power-up 3: Disminuye en 2 el multiplicador de velocidad de los enemigos (durante 8 segundos) cuando le des a la Z
+ *             - Power-up 4: El tiempo se detiene (durante 3 segundos) cuando le des a la X
+ *       - Los tres primeros power-ups que aparecen se pueden escoger en las opciones
+ *       - Todos los power-ups son acumulables
+ * - [ ] Implementar power-downs: Al colisionar con un power-down, se obtiene una penalización
+ *       - [ ] Cada 30 segundos (y durante 5 segundos), aparece un power-down en una posición aleatoria
+ *             - Power-down 1: Aumenta el radio del jugador (durante 10 segundos)
+ *             - Power-down 2: Aumenta el tamaño de los enemigos (durante 10 segundos)
+ *             - Power-down 3: El jugador se convierte en un cuadrado (durante 10 segundos)
+ *             - Power-down 4: Los enemigos tienen atracción gravitatoria hacia el jugador (durante 5 segundos), es decir, los enemigos se moverán un poquito hacia la posición del jugador
  * - [X] Implementar movimiento de los enemigos básico
  * - [ ] Implementar música (a medida que avanza el juego, a la música se le añaden más instrumentos)
  * - [ ] Implementar efectos visuales (ej: rastro del jugador)
