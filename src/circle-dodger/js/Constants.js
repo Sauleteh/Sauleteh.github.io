0e72f3b9fa42749c4aabfe142cebbfcf21504da0
Sauleteh <sauljgz21@yahoo.es>
2024-05-17 14:23:37 +0200

    Actualización de seguridad para el buscaminas y circle dodger

--- src/circle-dodger/js/Constants.js
+++ src/circle-dodger/js/Constants.js
@@ -1,4 +1,6 @@
 export const STORAGE_KEYS = { // Claves de almacenamiento local
     OPTION_CLICK_INSTEAD_OF_HOLDING: "optClickInsteadOfHoldingCD",
     OPTION_NAME: "optNameCD",
+    DEVICE_ID: "deviceID_long",
+    PASS: "userPass_long"
 };
