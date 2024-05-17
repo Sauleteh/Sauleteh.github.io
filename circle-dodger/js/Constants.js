bf79e24769907b0a6b759b9e7917554b35f0be76
Sauleteh <sauljgz21@yahoo.es>
2024-05-17 14:37:37 +0200

    Updates

--- circle-dodger/js/Constants.js
+++ circle-dodger/js/Constants.js
@@ -1,6 +1,6 @@
 export const STORAGE_KEYS = { // Claves de almacenamiento local
     OPTION_CLICK_INSTEAD_OF_HOLDING: "optClickInsteadOfHoldingCD",
-    OPTION_NAME: "optNameCD",
+    OPTION_NAME: "userName_long",
     DEVICE_ID: "deviceID_long",
     PASS: "userPass_long"
 };
