a2a352d1e08c7661f9feda7bd4d99de6161468a8
Sauleteh <sauljgz21@yahoo.es>
2024-07-31 00:37:24 +0200

    Página aboutme terminada

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 
 export function Aboutme() {
     const textAnimated = useRef(false);
+    let currentSection = 0;
 
     function handleLink(link) {
         window.open(link, "_blank");
@@ -31,8 +32,29 @@ export function Aboutme() {
         }, 20);
     }
 
+    function delayAnimations() {
+        const languageItems = document.querySelectorAll(".aboutme-language-item");
+        for (let i = 0; i < languageItems.length; i++) {
+            languageItems[i].style.animationDelay = `${i/4}s`;
+        }
+
+        const technologyItems = document.querySelectorAll(".aboutme-technology-item");
+        for (let i = 0; i < technologyItems.length; i++) {
+            technologyItems[i].style.animationDelay = `${i/4}s`;
+        }
+    }
+
+    function handleCarousel(direction) {
+        const sections = document.querySelectorAll(".aboutme-section");
+        
+        sections[currentSection].classList.remove("aboutme-section-active");
+        currentSection = (currentSection + direction + sections.length) % sections.length;
+        sections[currentSection].classList.add("aboutme-section-active");
+    }
+
     useEffect(() => {
         animateText();
+        delayAnimations();
     });
 
     return (
@@ -46,55 +68,66 @@ export function Aboutme() {
                 Si te ha gustado algún proyecto o te ha resultado útil, recuerda que puedes dejar una estrella en el repositorio de GitHub correspondiente.
             </p>
 
-            <h2 className="aboutme-title">Lenguajes que más uso</h2>
-            <ul className="aboutme-language-container">
-                <li className="aboutme-language-item">
-                    <img className="aboutme-language-icon" src="/logos/javascript-logo.svg"/><label className="aboutme-language">JavaScript</label>
-                </li>
-                <li className="aboutme-language-item">
-                    <img className="aboutme-language-icon" src="/logos/typescript-logo.svg"/><label className="aboutme-language">TypeScript</label>
-                </li>
-                <li className="aboutme-language-item">
-                    <img className="aboutme-language-icon" src="/logos/java-logo.svg"/><label className="aboutme-language">Java</label>
-                </li>
-                <li className="aboutme-language-item">
-                    <img className="aboutme-language-icon" src="/logos/kotlin-logo.svg"/><label className="aboutme-language">Kotlin</label>
-                </li>
-                <li className="aboutme-language-item">
-                    <img className="aboutme-language-icon" src="/logos/c-logo.svg"/><label className="aboutme-language">C</label>
-                </li>
-                <li className="aboutme-language-item">
-                    <img className="aboutme-language-icon" src="/logos/cpp-logo.svg"/><label className="aboutme-language">C++</label>
-                </li>
-                <li className="aboutme-language-item">
-                    <img className="aboutme-language-icon" src="/logos/csharp-logo.svg"/><label className="aboutme-language">C#</label>
-                </li>
-                <li className="aboutme-language-item">
-                    <img className="aboutme-language-icon" src="/logos/python-logo.svg"/><label className="aboutme-language">Python</label>
-                </li>
-            </ul>
+            <div className="aboutme-button-container">
+                <button className="aboutme-button aboutme-button-left" onClick={() => handleCarousel(-1)}><img className="aboutme-button-icon" src="/right-arrow.svg"/></button>
+                <button className="aboutme-button aboutme-button-right" onClick={() => handleCarousel(1)}><img className="aboutme-button-icon" src="/right-arrow.svg"/></button>
+            </div>
 
-            <h2 className="aboutme-title">Tecnologías más usadas en mis distintos proyectos</h2>
-            <ul className="aboutme-technology-container">
-                <li className="aboutme-technology-item">
-                    <img className="aboutme-technology-icon" src="/logos/react-logo.svg"/><label className="aboutme-technology">React</label>
-                </li>
-                <li className="aboutme-technology-item">
-                    <img className="aboutme-technology-icon" src="/logos/nodejs-logo.svg"/><label className="aboutme-technology">Node.js</label>
-                </li>
-                <li className="aboutme-technology-item">
-                    <img className="aboutme-technology-icon" src="/logos/express-logo.svg"/><label className="aboutme-technology">Express</label>
-                </li>
-                <li className="aboutme-technology-item">
-                    <img className="aboutme-technology-icon" src="/logos/mysql-logo.svg"/><label className="aboutme-technology">MySQL</label>
-                </li>
-                <li className="aboutme-technology-item">
-                    <img className="aboutme-technology-icon" src="/logos/websockets-logo.svg"/><label className="aboutme-technology">WebSockets</label>
-                </li>
-            </ul>
+            <div className="aboutme-section-container">
+                <div className="aboutme-section aboutme-section-active">
+                    <h2 className="aboutme-title">Lenguajes que más uso</h2>
+                    <ul className="aboutme-language-container">
+                        <li className="aboutme-language-item">
+                            <img className="aboutme-language-icon" src="/logos/javascript-logo.svg"/><label className="aboutme-language">JavaScript</label>
+                        </li>
+                        <li className="aboutme-language-item">
+                            <img className="aboutme-language-icon" src="/logos/typescript-logo.svg"/><label className="aboutme-language">TypeScript</label>
+                        </li>
+                        <li className="aboutme-language-item">
+                            <img className="aboutme-language-icon" src="/logos/java-logo.svg"/><label className="aboutme-language">Java</label>
+                        </li>
+                        <li className="aboutme-language-item">
+                            <img className="aboutme-language-icon" src="/logos/kotlin-logo.svg"/><label className="aboutme-language">Kotlin</label>
+                        </li>
+                        <li className="aboutme-language-item">
+                            <img className="aboutme-language-icon" src="/logos/c-logo.svg"/><label className="aboutme-language">C</label>
+                        </li>
+                        <li className="aboutme-language-item">
+                            <img className="aboutme-language-icon" src="/logos/cpp-logo.svg"/><label className="aboutme-language">C++</label>
+                        </li>
+                        <li className="aboutme-language-item">
+                            <img className="aboutme-language-icon" src="/logos/csharp-logo.svg"/><label className="aboutme-language">C#</label>
+                        </li>
+                        <li className="aboutme-language-item">
+                            <img className="aboutme-language-icon" src="/logos/python-logo.svg"/><label className="aboutme-language">Python</label>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="aboutme-section">
+                    <h2 className="aboutme-title">Tecnologías más usadas en mis distintos proyectos</h2>
+                    <ul className="aboutme-technology-container">
+                        <li className="aboutme-technology-item">
+                            <img className="aboutme-technology-icon" src="/logos/react-logo.svg"/><label className="aboutme-technology">React</label>
+                        </li>
+                        <li className="aboutme-technology-item">
+                            <img className="aboutme-technology-icon" src="/logos/nodejs-logo.svg"/><label className="aboutme-technology">Node.js</label>
+                        </li>
+                        <li className="aboutme-technology-item">
+                            <img className="aboutme-technology-icon" src="/logos/express-logo.svg"/><label className="aboutme-technology">Express</label>
+                        </li>
+                        <li className="aboutme-technology-item">
+                            <img className="aboutme-technology-icon" src="/logos/mysql-logo.svg"/><label className="aboutme-technology">MySQL</label>
+                        </li>
+                        <li className="aboutme-technology-item">
+                            <img className="aboutme-technology-icon" src="/logos/websockets-logo.svg"/><label className="aboutme-technology">WebSockets</label>
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
             <hr className="aboutme-line"/>
-            
+
             <ul className="aboutme-link-container">
                 <li className="aboutme-link-item" onClick={() => handleLink("https://github.com/Sauleteh")}>
                     <img className="aboutme-link-icon" src="/logos/github-mark-white.svg"/><label className="aboutme-link">Cuenta principal</label>
