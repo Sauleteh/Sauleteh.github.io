910d9e6b615e03b6bb21b06337d6cfaca3ec1c4f
Sauleteh <sauljgz21@yahoo.es>
2024-08-01 18:25:07 +0200

    Footer siempre en la parte inferior de la página

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -2,16 +2,16 @@ import "../css/components/FooterBar.css"
 
 export function FooterBar() {
     return (
-        <>
-        <hr className="footerbar-line"/>
-        <ul className="footerbar-link-container">
-            <li className="footerbar-link-item" onClick={() => handleLink("https://github.com/Sauleteh")}>
-                <img className="footerbar-link-icon" src="/logos/github-mark-white.svg"/><label className="footerbar-link">Cuenta principal</label>
-            </li>
-            <li className="footerbar-link-item" onClick={() => handleLink("https://github.com/UO279176")}>
-                <img className="footerbar-link-icon" src="/logos/github-mark-white.svg"/><label className="footerbar-link">Cuenta universitaria</label>
-            </li>
-        </ul>
-        </>
+        <div className="footerbar-body">
+            <hr className="footerbar-line"/>
+            <ul className="footerbar-link-container">
+                <li className="footerbar-link-item" onClick={() => handleLink("https://github.com/Sauleteh")}>
+                    <img className="footerbar-link-icon" src="/logos/github-mark-white.svg"/><label className="footerbar-link">Cuenta principal</label>
+                </li>
+                <li className="footerbar-link-item" onClick={() => handleLink("https://github.com/UO279176")}>
+                    <img className="footerbar-link-icon" src="/logos/github-mark-white.svg"/><label className="footerbar-link">Cuenta universitaria</label>
+                </li>
+            </ul>
+        </div>
     )
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,13 +2,13 @@ import "../css/components/NavigationBar.css"
 
 export function NavigationBar() {
     return (
-        <>
-        <nav className="navbar-nav">
-            <a href="/aboutme" className="navbar-item">Sobre mí</a>
-            <a href="/projects" className="navbar-item">Proyectos</a>
-            <a href="/shop" className="navbar-item">Tienda</a>
-        </nav>
-        <hr className="navbar-line"/>
-        </>
+        <div>
+            <nav className="navbar-nav">
+                <a href="/aboutme" className="navbar-item">Sobre mí</a>
+                <a href="/projects" className="navbar-item">Proyectos</a>
+                <a href="/shop" className="navbar-item">Tienda</a>
+            </nav>
+            <hr className="navbar-line"/>
+        </div>
     )
 }
