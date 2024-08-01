2770236f032e708cd6c83e52bf496116b5d1d996
Sauleteh <sauljgz21@yahoo.es>
2024-08-01 03:10:59 +0200

    FilterBar y SearchBar estilados

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -10,22 +10,26 @@ export function FilterBar({ sendData, projectData }) {
     }
 
     return (
-        <select className="filterbar-select" onChange={handleChange}>
-            <option value="all" data-type="all">Sin filtrado</option>
-            <optgroup label="Tema">
-                {
-                    themes.map((theme, index) => (
-                        <option key={index} value={theme.toLowerCase()} data-type="theme">{theme}</option>
-                    ))
-                }
-            </optgroup>
-            <optgroup label="Plataforma">
-                {
-                    platforms.map((platform, index) => (
-                        <option key={index} value={platform.toLowerCase()} data-type="platform">{platform}</option>
-                    ))
-                }
-            </optgroup>
-        </select>
+        <div className="filterbar-container">
+            <select className="filterbar-select" onChange={handleChange}>
+                <optgroup className="filterbar-optgroup" label="Filtrar por">
+                    <option className="filterbar-option" value="all" data-type="all">Sin filtrado</option>
+                </optgroup>
+                <optgroup className="filterbar-optgroup" label="Tema">
+                    {
+                        themes.map((theme, index) => (
+                            <option className="filterbar-option" key={index} value={theme.toLowerCase()} data-type="theme">{theme}</option>
+                        ))
+                    }
+                </optgroup>
+                <optgroup className="filterbar-optgroup" label="Plataforma">
+                    {
+                        platforms.map((platform, index) => (
+                            <option className="filterbar-option" key={index} value={platform.toLowerCase()} data-type="platform">{platform}</option>
+                        ))
+                    }
+                </optgroup>
+            </select>
+        </div>
     )
 }
