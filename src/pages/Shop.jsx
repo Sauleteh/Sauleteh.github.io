ca8c4845ced35259a0817d81559bdfd1afccddb7
Sauleteh <sauljgz21@yahoo.es>
2024-08-11 03:00:32 +0200

    Tienda: Añadido ayuda sobre obtención de objetos

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,16 +3,28 @@ import { UpgradeItem } from "../components/UpgradeItem";
 
 export function Shop() {
     const coinsData = {
-        banana: { image: "/coin-banana.gif", id: "coin_banana" },
-        msFlag: { image: "/coin-minesweeper.png", id: "coin_ms_flag" },
-        tetrisPiece: { image: "/coin-tetris.png", id: "coin_tetris_piece" }
+        banana: {
+            image: "/coin-banana.gif",
+            id: "coin_banana",
+            howToObtain: "¿Has encontrado algún plátano por la página?"
+        },
+        msFlag: {
+            image: "/coin-minesweeper.png",
+            id: "coin_ms_flag",
+            howToObtain: "Cada vez que termines una partida del buscaminas (excepto modo personalizado), por cada bandera puesta correctamente obtendrás este objeto"
+        },
+        tetrisPiece: {
+            image: "/coin-tetris.png",
+            id: "coin_tetris_piece",
+            howToObtain: "En el Tetris, cada vez que rompas una línea que tenga un cuadrado perteneciente a un palo (pieza azul) obtendrás este objeto"
+        }
     };
 
     // Si isSwitch es true, es obligatorio que func esté definido (func es la función que se ejecutará al cambiar el switch)
     const upgrades = [{
             title: "Tema claro",
-            description: "Quémate los ojos con este tema donde predomina el color blanco",
-            price: 1000,
+            description: "Quémate los ojos con este tema donde predomina el color blanco. ¡Cuidado con el precio! Cuanto más tiempo pase, más caro será.",
+            price: Math.floor((new Date().getTime() - new Date(2012, 2, 16).getTime()) / (1000 * 60 * 60 * 24)),
             coin: coinsData.banana,
             textWhenBought: "Activar tema claro",
             isSwitch: true,
@@ -20,16 +32,16 @@ export function Shop() {
             func: setLightTheme
         }, {
             title: "Tetris: Efecto especial tetris",
-            description: "En mi juego del Tetris he añadido dos easter egg: uno relacionado con hacer un tetris (romper cuatro líneas con una pieza) y otro relacionado con la música. Compra este item para saber cómo activar los easter egg",
-            price: 60,
+            description: "En mi juego del Tetris he añadido dos easter egg: uno relacionado con hacer un tetris (romper cuatro líneas con una pieza) y otro relacionado con la música. Compra este item para saber cómo activar los easter egg.",
+            price: 80,
             coin: coinsData.tetrisPiece,
-            textWhenBought: "Realiza el código Konami en la página del Tetris y aparecerá una opción 'Opción experimental'. Actívala y los easter egg estarán activados",
+            textWhenBought: "Realiza el código Konami en la página del Tetris y aparecerá una opción 'Opción experimental'. Actívala y los easter egg estarán activados.",
             isSwitch: false,
             id: "upgrade_tetris_special"
         }, {
             title: "Buscaminas: Modo discoteca",
-            description: "Si te pasas una dificultad con este modo especial y haces un buen tiempo, tu nombre aparecerá con color de fuego en el scoreboard. Ten en cuenta que este modo es más difícil que el buscaminas normal, compra este item para saber cómo activar este easter egg y sabrás a qué me refiero",
-            price: 250,
+            description: "Si te pasas una dificultad con este modo especial y haces un buen tiempo, tu nombre aparecerá con color de fuego en el scoreboard. Ten en cuenta que este modo es más difícil que el buscaminas normal, compra este item para saber cómo activar este easter egg y sabrás a qué me refiero.",
+            price: 350,
             coin: coinsData.msFlag,
             textWhenBought: "Realiza el código Konami en la página del Buscaminas y aparecerá una opción '???'. Para activarla, primero mantén la tecla 'Control' izquierdo, luego mantén la tecla 'º' y haz click sobre la opción para activarla (No dejes de mantener ninguna de las teclas hasta que hagas click).",
             isSwitch: false,
@@ -41,20 +53,27 @@ export function Shop() {
         console.log("holas");
     }
 
+    function handleHelpClick(howToObtain) {
+        const helpText = document.querySelector(".shop-coin-help-text");
+        helpText.textContent = howToObtain;
+    }
+
     return (
         <div className="shop-body">
             <h1 className="shop-title">Tienda</h1>
             <h2 className="shop-subtitle">Compra mejoras para la página</h2>
             <div className="shop-coins-container">
                 {Object.values(coinsData).map((coin, index) => (
-                    <div key={index} className="shop-coin-container">
+                    <div key={index} className="shop-coin-container" onClick={() => handleHelpClick(coin.howToObtain)}>
                         <img className="shop-coin-image" src={coin.image}/>
                         <p className="shop-coin-count">{localStorage.getItem(coin.id) === null || localStorage.getItem(coin.id) === "" ? 0 : localStorage.getItem(coin.id)}</p>
                     </div>
                 ))}
             </div>
-            <button>¿Cómo obtener objetos?</button>
-            <p>Mejoras disponibles:</p>
+            <div className="shop-coin-help-container">
+                <img className="shop-coin-help-image" src="/question-mark.png"/>
+                <p className="shop-coin-help-text">Selecciona un objeto para saber cómo obtenerlo</p>
+            </div>
             <div className="shop-upgrades-container">
                 {upgrades.map((upgrade, index) => (
                     <UpgradeItem key={index}
