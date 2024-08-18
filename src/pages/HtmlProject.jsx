5ffdaa01b37d37ef7cd5bae13e95e0fa37f351d9
Sauleteh <sauljgz21@yahoo.es>
2024-08-18 17:02:49 +0200

    Separación de temas CSS + adaptación modo claro a todas las páginas

--- src/pages/HtmlProject.jsx
+++ src/pages/HtmlProject.jsx
@@ -2,8 +2,14 @@ import "../css/pages/HtmlProject.css"
 import PropTypes from "prop-types"
 
 export function HtmlProject({ url }) {
+    function handleLightTheme(event) {
+        // Control del modo claro/oscuro para el HTML del iframe
+        const element = event.target.contentDocument.querySelector("html");
+        if (localStorage.getItem("page_light_theme") === "true") element.setAttribute("data-theme", "light");
+        else element.removeAttribute("data-theme");
+    }
     return (
-        <iframe className="htmlproject-iframe" src={url} />
+        <iframe className="htmlproject-iframe" src={url} onLoad={handleLightTheme}/>
     )
 }
 
